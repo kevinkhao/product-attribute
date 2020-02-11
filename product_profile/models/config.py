c783c412863b5d93fcdda2012024f7f212017f02
Kevin Khao <kevinkhao@gmail.com>
2020-02-11 10:55:01 +0100

    [IMP] black, structure change

--- product_profile/models/config.py
+++ product_profile/models/config.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 # © 2015 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
@@ -7,10 +6,11 @@ from .product import PROFILE_MENU
 
 
 class BaseConfigSettings(models.TransientModel):
-    _inherit = 'base.config.settings'
+    _inherit = "base.config.settings"
 
     group_product_profile = fields.Boolean(
         string="Display Product Profile fields",
-        implied_group='product_profile.group_product_profile',
+        implied_group="product_profile.group_product_profile",
         help="Display fields computed by product profile "
-             "module.\nFor debugging purpose see menu\n%s" % PROFILE_MENU)
+        "module.\nFor debugging purpose see menu\n%s" % PROFILE_MENU,
+    )
